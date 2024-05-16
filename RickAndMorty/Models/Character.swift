4e9c010f3174ede971fcaf736e111ab3cbbe2f75
Phoenix-1452 <sadovodov23092002@mail.ru>
2024-05-16 20:32:23 +0300

    add models

--- RickAndMorty/Models/Character.swift
+++ RickAndMorty/Models/Character.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+// MARK: - Character
+struct Character: Codable {
+    let id: Int
+    let name: String
+    let status: CharacterStatus
+    let species: String
+    let type: String
+    let gender: CharacterGender
+    let origin: Origin
+    let location: SingleLocation
+    let image: String
+    let episode: [String]
+    let url: String
+    let created: String
+}
+
+// MARK: - Location
+struct SingleLocation: Codable {
+    let name: String
+    let url: String
+}
+
+// MARK: - Origin
+struct Origin: Codable {
+    let name: String
+    let url: String
+}
+
+// MARK: - CharacterStatus
+enum CharacterStatus: String, Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case unknown = "unknown"
+}
+
+// MARK: - CharacterStatus
+enum CharacterGender: String, Codable {
+    case male = "Male"
+    case female = "Female"
+    case genderless = "Genderless"
+    case unknown = "unknown"
+}
+
+
